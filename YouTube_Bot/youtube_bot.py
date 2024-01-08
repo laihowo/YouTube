b3faff9127ae37f98abb81261742fa8bf2df1c26
Benny LAI <howo0707@gmail.com>
2024-01-08 19:30:31 +0000

    Add parallel programming with bot server

--- YouTube_Bot/youtube_bot.py
+++ YouTube_Bot/youtube_bot.py
@@ -1,10 +1,28 @@
+'''
+Runs the YouTube Bot as a FastAPI app.
+'''
+
+import os, threading
 from dotenv import dotenv_values
 
 import fastapi_poe as fp
 from yt_bot import YTSummarizerBot
 
+def fetch_settings(key):
+    '''Fetches the settings for the bot.'''
+    os.system(f'sleep 5 && \
+        curl -X POST https://api.poe.com/bot/fetch_settings/YouTubeAgent/{key}')
+
 def fastapi_app():
+    '''Parallel programming with the bot server.'''
+
+    key = dotenv_values()['POE_ACCESS_KEY']
+
+    # Create a thread that will run the fetch_settings function
+    settings_thread = threading.Thread(target=fetch_settings, args=(key,))
+    settings_thread.start()
+
     bot = YTSummarizerBot()
-    fp.run(bot, access_key=dotenv_values()['POE_ACCESS_KEY'])
+    fp.run(bot, access_key=key)
 
 fastapi_app()
